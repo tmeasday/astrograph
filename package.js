fe8c399489315abc6190ee645a72d38ca615880c
Tom Coleman <tom@thesnail.org>
2013-02-21 12:09:51 +1100

    Renamed, fixed slightly.

--- package.js
+++ package.js
@@ -3,7 +3,6 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.use('autopublish', 'client');
   api.use('templating', 'client');
   
   api.add_files(['data_browser.html', 'data_browser.js', 'data_browser.css'], 'client');
